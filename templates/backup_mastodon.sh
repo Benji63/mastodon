d62e142895d53258a1ade238566f982b414f267b
Benji63 <piat9871@gmail.com>
2023-02-26 15:30:54 +0100

    Uniformisation de tous les noms de taches

--- templates/backup_mastodon.sh
+++ templates/backup_mastodon.sh
@@ -3,11 +3,11 @@ set -eo pipefail
 
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 
-# Export database, compress, and send to file with today's date
+# Exporter la base de données, compresser et envoyer dans un fichier avec la date d'aujourd'hui
 sudo -u postgres pg_dump mastodon_production | gzip > \
   /home/mastodon/backups/database/mastodon_production-"$NOW".sql.gz
 
-# Copy redis automatic backup
+# Copier la sauvegarde automatique redis
 cp /var/lib/redis/dump.rdb /home/mastodon/backups/redis/redis-"$NOW".rdb
 
 touch /tmp/backup
